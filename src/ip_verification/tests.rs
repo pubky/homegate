dae00927b784994a7c7e2bec14d1001742cce5c7
tomos <tomos@synonym.to>
2026-05-15 13:09:45 +0100

    feat: Add allowed_write_paths to ip_verification.signup_quota config

--- src/ip_verification/tests.rs
+++ src/ip_verification/tests.rs
@@ -331,6 +331,7 @@ async fn test_signup_quota_uses_post_endpoint(pool: PgPool) {
         rate_read_burst: None,
         rate_write: Some("1mb/s".to_string()),
         rate_write_burst: None,
+        allowed_write_paths: None,
     };
     let service = create_service_with_quota(&servers, db, 2, 10, Some(quota));
     let ip: IpAddr = "10.0.0.1".parse().unwrap();
